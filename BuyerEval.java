4f54badffbde5e844d4077b05e89ad940c4cc092
Sarah <sarahtvana@gmail.com>
2023-11-04 23:30:45 -0500

    Working approval calcuations, rejection reasons, warnings

--- BuyerEval.java
+++ BuyerEval.java
@@ -25,18 +25,87 @@ public class BuyerEval {
                 Entry tempEntry = new Entry(Integer.parseInt(tempArr[0]), Double.parseDouble(tempArr[1]), Double.parseDouble(tempArr[2]),
                 Double.parseDouble(tempArr[3]),Double.parseDouble(tempArr[4]), Double.parseDouble(tempArr[5]), Double.parseDouble(tempArr[6]),
                 Double.parseDouble(tempArr[7]),Double.parseDouble(tempArr[8]), Double.parseDouble(tempArr[9]));
+                calculateApproval(tempEntry);
+                
+                //Add entry to list of entries
+                homeBuyers.add(tempEntry);
 
-              //Add entry to list of entries
-              homeBuyers.add(tempEntry);
+                // call approve method and pass the tempentry
+                //boolean approved = calculateApproval(tempEntry);
+                
             }
             br.close();
-    }
+        }
         catch(IOException ioe){
             ioe.printStackTrace();
         }
+
         //print to see if it works
-        for (int i = 0; i < 3; i++){
-            System.out.println(homeBuyers.get(i).toString());
+        for (int i = 200; i < 300; i++){
+            System.out.println(homeBuyers.get(i).entryToString());
+        }
+    }
+
+    // approve method that takes the entry
+    public boolean calculateApproval(Entry myEntry){
+        if (checkCreditScore(myEntry) && checkLTV(myEntry) && checkDTI(myEntry)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkCreditScore(Entry myEntry) {
+        if (myEntry.creditScore <= 640){
+            myEntry.addRejectionReason(0, "Low credit score");
+        }
+        return myEntry.creditScore >= 640;
+    }
+
+    public boolean checkLTV(Entry myEntry) {
+        // LTV = % after doing (appraisedValue - downPayment) / appraisedValue
+        // if LTV is <80%, you're fine
+        // if LTV is 80-95%, might qualify but you'd have to purchase PMI
+        // PMI will add a small yearly % to the amt you pay for the house
+        double LTV = (myEntry.appraisedValue - myEntry.downPayment) / myEntry.appraisedValue;
+        if (LTV < 0.8) {
+            return true;
+        }
+        else if (LTV >= 0.8 && LTV <= 0.95) {
+            myEntry.addWarnings(0, "LTV % is high, you will probably have to purchase PMI \n" + 
+            "if you are approved, or you may be rejected");
+            return true;
+        }
+        else {
+            myEntry.addRejectionReason(1, "LTV % too high");
+            return false;
+        }
+    }
+    
+    public boolean checkDTI(Entry myEntry){
+        double monthlyDebt = myEntry.carPayment + myEntry.creditCardPayment + myEntry.monthlyMortgagePayment;
+        double dti = (myEntry.grossMonthlyIncome / monthlyDebt) * 100;
+        
+        double fedti = (myEntry.grossMonthlyIncome / myEntry.monthlyMortgagePayment) * 100;
+
+        //DTI can't be more than 43, but less than 38 is ideal
+        //FEDTI can't be more than 28
+        if (dti > 43 && fedti > 28){ // if both are invalid
+            myEntry.addRejectionReason(2, "DTI and FEDTI % too high");
+            return false;
+        }
+        else if (dti > 43 && fedti <= 28){ // if dti is invalid and fedti is valid
+            myEntry.addRejectionReason(2, "DTI % too high");
+            return false;
+        }
+        else if (fedti > 28 && dti <= 43){ // if fedti is invalid and dti is valid
+            myEntry.addRejectionReason(2, "FEDTI % too high");
+            return false;
+        }
+        else {
+            if (dti > 38){ //lenders typically accept no more than 38%
+                myEntry.addWarnings(1, "DTI outside of preferred range");
+            }     
+            return true;
         }
     }
 }
--- Entry.java
+++ Entry.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Entry {
     int id;
     double grossMonthlyIncome;
@@ -9,7 +12,11 @@ public class Entry {
     double loanAmount;
     double monthlyMortgagePayment;
     double creditScore;
-    
+    private boolean approved;
+    private String[] rejectionReason = {"", "", ""};
+    private String[] warnings = {"", "", ""};
+
+    //--- CONSTRUCTORS ---
     public Entry(int id, double grossMonthlyIncome, double creditCardPayment, double carPayment, double studentLoanPayments, double appraisedValue,
                     double downPayment, double loanAmount, double monthlyMortgagePayment, double creditScore){
     
@@ -23,13 +30,52 @@ public class Entry {
         this.loanAmount = loanAmount;
         this.monthlyMortgagePayment = monthlyMortgagePayment;
         this.creditScore = creditScore;
+
     }
 
-    @Override
-    public String toString(){
-        return "id:  "+ id+ "| grossMonthlyIncome: " +grossMonthlyIncome + "| creditCardPayment: " + creditCardPayment +
-         "| carPayment: " + carPayment + "| studentLoanPayments: " +studentLoanPayments + "| appraised value: " + appraisedValue
-         + "| down payment: " + downPayment + "| loan amount: " + loanAmount + "| monthly mortgage payment:" + monthlyMortgagePayment
-         + "| credit score: " + creditScore + "\n\n";
+    //--- GETTERS & SETTERS ---
+
+    //approval status
+    public boolean getApproved(){
+        return this.approved;
+    }
+    public void setApproved(boolean approved){
+        this.approved = approved;
+    }
+
+    //rejection reasons
+    public String[] getRejectionRason(){
+        return this.rejectionReason;
+    }
+    public void setRejectionReason(String[] rejectionReason){
+         this.rejectionReason = rejectionReason;
+    }
+    public void addRejectionReason(int position, String rejectionReason){
+        this.rejectionReason[position] = rejectionReason;
+    }
+
+    //warnings
+    public String[] getWarnings(){
+        return this.warnings;
+    }    
+    public void addWarnings(int position, String warning){
+        this.warnings[position] = warning;
+    }
+    
+    //--- METHODS ---
+    public String entryToString(){
+        return "id : "+ id+ "\ngrossMonthlyIncome: " +grossMonthlyIncome + "\ncreditCardPayment: " 
+        + creditCardPayment + "\ncarPayment: " + carPayment + "\nstudentLoanPayments: " 
+        + studentLoanPayments + "\nappraised value: " + appraisedValue + "\ndown payment: " 
+        + downPayment + "\nloan amount: " + loanAmount + "\nmonthly mortgage payment:" 
+        + monthlyMortgagePayment + "\ncredit score: " + creditScore + "\napproved: " + approved 
+        + "\nrejectionReason: " + rejectionToString() + "\nwarnings: " + warningToString() + "\n\n";
+    }
+
+    public String rejectionToString(){
+        return rejectionReason[0] + ", " + rejectionReason[1] + ", " + rejectionReason[2];
+    }
+    public String warningToString(){
+        return warnings[0] + ", " + warnings[1];
     }
 }
